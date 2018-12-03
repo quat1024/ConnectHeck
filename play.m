1ba40057dbd9713a11552ff0d436c1cecce22cc0
quat1024 <scratch933@gmail.com>
2018-12-03 18:13:40 -0500

    Arrays start at 1 :joy: :gun:

--- play.m
+++ play.m
@@ -21,7 +21,7 @@ function [newBoard] = play(board, player, aiControlled)
                 fprintf('Sorry, couldn''t hear you, come again?\n')
             elseif column ~= round(column, 0)
                 fprintf('Enter a whole number lol\n')
-            elseif column < 0 || column > boardWidth
+            elseif column < 1 || column > boardWidth
                 fprintf('Enter a number between 1 and %.0f, please\n', boardWidth)
             elseif board(1, column) ~= 0
                 fprintf('That column is already full of pieces!\n');
